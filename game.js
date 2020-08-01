d538f7df482af12b282f3dd588e9a1f3d1b9b045
supraja-puppala <puppalavamsireddy@gmail.com>
2020-08-01 18:51:21 +0530

    6

--- game.js
+++ game.js
@@ -5,25 +5,26 @@ var guessnumber = Math.round(Math.random() * 100);
 var lives = 10;
 var message;
 
-$submitbutton.onclick() => {
-    let userinput = document.getElementById("number-input").nodeValue;
+$submitbutton.onclick = () => {
+    let x = document.getElementById("number-input").value
+    console.log(x);
+    console.write(guessnumber);
     lives--;
-    if (userinput == guessnumber) {
+    if (x == guessnumber) {
         location.href = "./win";
     } else if (lives == 0) {
         location.href = "./lose";
-    } else if (userinput > guessnumber) {
-        message = 'Oops! you guess is too high!. You have'
-        $(lives)
-        'lives remaining';
-    } else if (userinput < guessnumber) {
-        message = 'Oops! you guess is too low!. You have'
-        $(lives)
-        'lives remaining';
+    } else if (x > guessnumber) {
+        console.log("You are high");
+        document.write("your guess is high");
+
+    } else if (x < guessnumber) {
+        console.log("you are low")
+        document.write("your guess is low");
+
+
     }
-    $message.style.display = "inherit";
-    $message.innerHTML = message
 
-}
 
-}
+
+};
---
+++ reset.js
@@ -0,0 +1,4 @@
+const $resetbutton = document.getElementById("rest")
+$resetbutton.onclick() => {
+    location.href = "./game.html";
+}
